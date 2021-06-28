db704b027ed17f4642ac48ef6ca0bbed8186c3e8
richu101 <richubini101@gmail.com>
2021-06-28 14:06:39 +0900

    test 3

--- Blink_LED/blink.c
+++ Blink_LED/blink.c
@@ -15,10 +15,13 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
+        sleep_ms(200);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(200);
+        gpio_put(LED_PIN, 1);
         sleep_ms(100);
         gpio_put(LED_PIN, 0);
         sleep_ms(100);
-        
     }
 #endif
 }
